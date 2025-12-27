2c99a116a95741880ddbe1df5e5b4bffb59c5aec
MivasPrince <prince@miva.university>
2025-12-27 09:41:38 +0100

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,15 +1,4 @@
 """Configuration package for MIND Platform"""
 
-from .database import get_bigquery_client, run_query, get_cached_query, DATASET_ID
-from .auth import UserRole, USERS, get_user_permissions, can_access_page
-
-__all__ = [
-    'get_bigquery_client',
-    'run_query', 
-    'get_cached_query',
-    'DATASET_ID',
-    'UserRole',
-    'USERS',
-    'get_user_permissions',
-    'can_access_page'
-]
+# Don't pre-import - let modules import directly to avoid circular imports
+__all__ = []
